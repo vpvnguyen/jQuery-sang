a388b253eb5031f91e6f644cfaac6e2e5b7bdee8
Vincent Nguyen <vpvnguyen@gmail.com>
2019-11-13 11:35:31 -0800

    on click and axios

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,25 @@
-$('#do-something').on('click', function () {
-    alert('hello!')
-})
+// interact with dom using id
+$("#do-something").on("click", function() {
+  alert("hello!");
+});
+
+// with classes
+$(".do-something-else").on("click", function() {
+  alert("heyo!");
+});
+
+// get data from backend
+$(".get-data").on("click", function() {
+
+  axios
+  .get("/api")
+  .then(function(response) {
+    console.log(response);
+    $('#put-data-here').append(`<div>${response.data.name}`);
+    $('#put-data-here').append(`<div>${response.data.title}`);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+});
