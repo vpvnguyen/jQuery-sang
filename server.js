4e044e390d0a2a9d7ce42b440df19a71ecb34faa
Vincent Nguyen <vpvnguyen@gmail.com>
2019-11-13 11:35:42 -0800

    api routing

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 // handle url encoded data; parse json
@@ -9,6 +8,17 @@ app.use(express.json());
 
 // make public static
 app.use(express.static('public'));
+
+app.get('/api', function(req, res) {
+    console.log('api route');
+
+    const data = {
+        name: 'Sang',
+        title: 'Developer'
+    };
+
+    res.json(data);
+});
 
 app.listen(PORT, function () {
     console.log(`Server is listening on PORT: ${PORT}`)
